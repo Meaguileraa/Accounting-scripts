e06904f75ef44be8ed065a3170d07a01ff11d403
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-13 19:33:13 -0700

    Initial Commit

---
+++ melon_info.py
@@ -0,0 +1,12 @@
+"""Print out all the melons in our inventory."""
+
+
+from melons import melons_dict
+
+
+def print_melon(melons_dict):
+    """Print each melon with corresponding attribute information."""
+
+
+
+print_melon(melons_dict)
