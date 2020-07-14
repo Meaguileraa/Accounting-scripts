abb8457b482cc3a6c097a909b2d91b3981ed99bc
Marissa Aguilera <marissaguilera@yahoo.com>
2020-07-13 19:34:30 -0700

    Final Code

--- melon_info.py
+++ melon_info.py
@@ -7,6 +7,9 @@ from melons import melons_dict
 def print_melon(melons_dict):
     """Print each melon with corresponding attribute information."""
 
-
+    for name, traits in melons_dict.items():
+        print(name.upper())
+        for traits, value in traits.items():
+            print(traits, ":", value)
 
 print_melon(melons_dict)
